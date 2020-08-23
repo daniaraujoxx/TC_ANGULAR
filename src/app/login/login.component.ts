0704cf9a82de2b926aed8a7e5b2ec14ed7235a1b
daniaraujoxx <danyeladearaujo@hotmail.com>
2020-08-23 15:27:38 -0300

    API COM ERRO

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { LoginService } from './login/shared/login.service';
+import { NgForm } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Login } from './login/shared/login.model';
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,6 +13,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  @ViewChild('formLogin', {static: true}) formLogin: NgForm;
+
+  request: Login = { 
+    status: number,
+    mensagem: string,
+    retorno: {
+        IdOperador: number,
+        NmOperador: string,
+        NrCPF: string,
+        NrMatricula: number,
+        DsCargo: string,
+        CdFilial: number,
+        PwOperador: string
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
