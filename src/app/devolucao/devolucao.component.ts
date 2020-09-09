22694ced97314444028c741cf4ede8cdb9c7ff98
daniaraujoxx <danyeladearaujo@hotmail.com>
2020-09-09 10:57:58 -0300

    Devolucao radios

--- src/app/devolucao/devolucao.component.ts
+++ src/app/devolucao/devolucao.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, TestabilityRegistry } from '@angular/core';
 import { NfResponse } from "./shared/nfResponse.model";
 import { DevolucaoService } from "./shared/devolucao.service";
 import { stringify } from '@angular/compiler/src/util';
+import { NF } from './shared/nf.model';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-devolucao',
@@ -11,11 +14,44 @@ import { stringify } from '@angular/compiler/src/util';
 export class DevolucaoComponent implements OnInit {
 
   @ViewChild('inputNf') nrNfElement: ElementRef;
+  @ViewChild('dtCupom') dataCupom: ElementRef;
 
-  nfResponse: NfResponse;
+
+  nf: NF = {
+
+    idDocumentoFiscal: null,
+    operacao: null,
+    filial: null,
+    cliente: null,
+    motivo: null,
+    idDocumentoFiscalVenda: null,
+    nrItem: null,
+    dataAbertura: null,
+    dataFechamento: null,
+    flagNota: null,
+    valorDocumento: null,
+    numeroCaixa: null,
+    itens: [],
+    tipoPagamento: null,
+    notaDevolvida: null
+
+  };
 
 
-  constructor(private devolucaoService: DevolucaoService) { }
+
+  nfResponse: NfResponse = {
+    status: null,
+    mensagem: null,
+    retorno: this.nf
+  };
+
+
+  tipoDevolucao = true;
+
+  constructor(private devolucaoService: DevolucaoService, public datepipe: DatePipe) {
+
+
+  }
 
 
   ngOnInit(): void {
@@ -26,8 +62,11 @@ export class DevolucaoComponent implements OnInit {
 
     this.devolucaoService.getNotaFiscal(dados).subscribe(response => {
       this.nfResponse = response;
+      this.dataCupom.nativeElement.value = this.datepipe.transform(this.nfResponse.retorno.dataAbertura, 'yyyy-MM-dd');
       console.log(this.nfResponse);
     })
 
+
   }
+  
 }
